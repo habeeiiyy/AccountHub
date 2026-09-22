9917d47e07d4bb6b36fcf0682b7717b55738ecec
habeeiiyy <habeebdeveloper.india@gmail.com>
2026-09-22 08:44:12 +0000

    next edit

--- Accounts/views.py
+++ Accounts/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache 
 
-# Create your views here.
+
+@never_cache 
+def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("accounts:home")
+
+    if request.method=="POST":
+        username=request.POST.get("Username")
+        password=request.POST.get("password")
+
+        user=authenticate(
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request,user)
+            return redirect("Accounts:home")
+        return render(
+            request,"Accounts/login.html",
+            {"error":"invalid username or password"}
+        )
+    return render("request","Accounts/login.html")
+
+@never_cache
+@login_required
+def home(request):
+    return render(request,"Accounts/home.html")
+
+@never_cache
+def logout_view(request):
+    logout(request)
+    return redirect("Accounts:login")
